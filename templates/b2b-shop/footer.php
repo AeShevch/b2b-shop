bc045fd180c2547f220326b65ce5792fec5ad3f4
ae.shevch <shevch.ae@gmail.com>
2021-01-25 20:09:05 +0300

    feat: includes scripts and styles in the template

--- templates/b2b-shop/footer.php
+++ templates/b2b-shop/footer.php
@@ -1,3 +1,6 @@
-
+<?php
+// Основные скрипты шаблона
+$this->addExternalJS(SITE_TEMPLATE_PATH . "/dist/bundle.min.js");
+?>
 </body>
 </html>
--- templates/b2b-shop/header.php
+++ templates/b2b-shop/header.php
@@ -1,21 +1,24 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 $curPage = $APPLICATION->GetCurPage(true);
-
 ?>
 
 <html xml:lang="<?= LANGUAGE_ID ?>" lang="<?= LANGUAGE_ID ?>">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><? $APPLICATION->ShowTitle() ?></title>
-    <link rel="shortcut icon" type="image/x-icon" href="<?= SITE_DIR ?>favicon.ico"/>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><? $APPLICATION->ShowTitle() ?></title>
+  <link rel="shortcut icon" type="image/x-icon" href="<?= SITE_DIR ?>favicon.ico"/>
     <? $APPLICATION->ShowHead(); ?>
+    <?php
+    // Основные стили шаблона
+    $this->addExternalCss(SITE_TEMPLATE_PATH . "/dist/bundle.min.css");
+    ?>
 </head>
 <body>
-    <div id="panel"><? $APPLICATION->ShowPanel(); ?></div>
+<div id="panel"><? $APPLICATION->ShowPanel(); ?></div>
 
-    <?
-    // Header
-    $APPLICATION->IncludeFile(SITE_TEMPLATE_PATH."/layouts/header.php"); ?>
+<?
+// Header
+$APPLICATION->IncludeFile(SITE_TEMPLATE_PATH . "/layouts/header.php"); ?>
